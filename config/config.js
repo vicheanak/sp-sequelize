08b7695687ca3cca0bf15e27618ba1f4ef583585
Vicheanak <vicheanak@gmail.com>
2016-10-01 20:10:22 +0700

    remove passport

--- config/config.js
+++ config/config.js
@@ -17,15 +17,6 @@ var computedConfig = {
     modelsDir : rootPath + '/app/models'
 };
 
-//
-// Setup nconf to use (in-order):
-//   1. Locally computed config
-//   2. Command-line arguments
-//   3. Some Environment variables
-//   4. Some defaults
-//   5. Environment specific config file located at './env/<NODE_ENV>.json'
-//   6. Shared config file located at './env/all.json'
-//
 nconf.argv()
     .env(['PORT','NODE_ENV','FORCE_DB_SYNC','forceSequelizeSync'])// Load select environment variables
     .defaults({store:{
